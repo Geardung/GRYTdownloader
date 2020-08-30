96056990160ed612ea40eae5b91ac23241c38109
Geardung <github.passis228sherdon@mail.ru>
2020-08-30 13:07:42 +0300

    new update

--- GRYouTubeScript.js
+++ GRYouTubeScript.js
@@ -4,6 +4,7 @@ const fs = require('./node_modules/fs-extra');
 
 var dir = './results';
 
+
 if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
 }
@@ -17,29 +18,49 @@ function start() {
                     ytdl.getInfo(answer).then(video => {
                         let result = `${video.videoDetails.title}\nРезультаты : \n`
                         let videoversionint = video.formats.length
-                        for (let index = 0; index < videoversionint; index++) {
-                            let audqual = video.formats[index].audioQuality
-                            if (audqual == `AUDIO_QUALITY_LOW`) {
-                                audqual = [`Низкое качество звука`, 2]
-                            } else if (audqual == `AUDIO_QUALITY_MEDIUM`) {
-                                audqual = [`Нормальное качество звука`, 3]
-                            } else if (audqual == undefined) audqual = [`Без звука`, 1]
-                            let videoquality = video.formats[index].quality
-                            result = `\t${result}${index + 1}. ${videoquality} - ${audqual[0]} - \t${video.formats[index].url}\t`
-                        }
-                        fs.writeFile(`results/${video.videoDetails.title}.txt`, result, (err) => {
-                            if (err) {
-                                console.log("Название видео невозможно использовать в качестве названия файла | Результаты сохранены в файле YTVideoList.txt")
-                                fs.writeFile(`results/YTVideoList.txt`, result, (err) => {
-                                    if (err) throw Error;
-
-                                });
-                                resolve(true)
-                            } else {
-                                console.log("Всё хорошо!\n" + video.videoDetails.title)
-                                resolve(true)
+                        new Promise((resolve) => {
+                            for (let index = 0; index < videoversionint; index++) {
+                                //      AUDIOPARSING
+                                //
+                                let audqual = video.formats[index].audioQuality
+                                if (audqual == `AUDIO_QUALITY_LOW`) {
+                                    audqual = [`Низкое качество звука`, 2]
+                                } else if (audqual == `AUDIO_QUALITY_MEDIUM`) {
+                                    audqual = [`Нормальное качество звука`, 3]
+                                } else if (audqual == undefined) audqual = [`Только видео`, 1]
+                                //      VIDEOPARSING
+                                //
+                                let videoquality = video.formats[index].quality
+                                if (videoquality == "large") {
+                                    videoquality = "Большое"
+                                } else if (videoquality == "medium") {
+                                    videoquality = "Среднее"
+                                } else if (videoquality == "small") {
+                                    videoquality = "Маленькое"
+                                } else if (videoquality == "tiny") {
+                                    videoquality = "Только звук"
+                                }
+                                //      SAVING RESULTS
+                                //
+                                result = `${result}${index + 1}. ${videoquality} - ${audqual[0]} - \n${video.formats[index].url}\n`
+                                if (index <= videoversionint) { resolve(true) }
                             }
-                        });
+                        }).then(() => {
+
+                            fs.writeFile(`results/${video.videoDetails.title}.txt`, result, (err) => {
+                                if (err) {
+                                    console.log("Название видео невозможно использовать в качестве названия файла!\n Результаты сохранены в файле YTVideoList.txt")
+                                    fs.writeFile(`results/YTVideoList.txt`, result, (err) => {
+                                        if (err) throw Error;
+
+                                    });
+                                    resolve(true)
+                                } else {
+                                    console.log("Всё хорошо!\n" + video.videoDetails.title)
+                                    resolve(true)
+                                }
+                            });
+                        })
                     })
                 } else if (answer != undefined) {
                     console.log(`Не распознана ссылка YouTube!`)
--- start.cmd
+++ start.cmd
@@ -1,6 +1,5 @@
 @echo off
 title YouTubeDownloader by Geardung
-mkdir results
 cls
 node GRYouTubeScript.js
 pause
